39c27dcb33726dbfb610536f78b550c67dc51b99
hkx <1441609949@qq.com>
2019-01-23 16:21:52 +0800

    理解中间件中next函数

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,8 @@
 // import { createStore } from "redux";
 import { createStore,applyMiddleware } from "./my-redux";
 import { guns } from "./actions";
-//在中间件中，next参数是在使用compose组合函数时，后面的中间件装饰好后的dispatch，如果不调用next，那么后面的中间件都不会执行
+//在中间件中，next参数是在使用compose组合函数时，后面的中间件装饰好后的dispatch。如果不调用next，那么后面的中间件都不会执行
+//在logger中，next = (action)=>{logger2(store)(store.dispatch(action))} 如果
 const logger = store=>next=>action=>{
     store.dispatch(action)
     console.log('第一个中间价');
